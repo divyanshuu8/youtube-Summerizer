c0c59408dcfc09927083276159d9591f51fa1c18
divyanshuu8 <singhdivyanshu975@gmail.com>
2025-01-07 20:19:37 +0530

    done

--- frontend/src/components/auth/nhost.js
+++ frontend/src/components/auth/nhost.js
@@ -1,8 +1,9 @@
 import { NhostClient } from '@nhost/nhost-js';
 
+// Use environment variables for the subdomain and region
 const nhost = new NhostClient({
-  subdomain: 'yppwpzvsiylmuphjmdfk', // e.g., abc123
-  region: 'ap-south-1'       // e.g., eu-central-1
+  subdomain: import.meta.env.VITE_NHOST_SUBDOMAIN, // Get subdomain from .env
+  region: import.meta.env.VITE_NHOST_REGION        // Get region from .env
 });
 
 export default nhost;
